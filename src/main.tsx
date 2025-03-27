a6d018d01977270090bb665ea681b4fb3d32497c
Morty <drakozack31@gmail.com>
2025-03-26 21:49:02 -0600

    deploy config

--- src/main.tsx
+++ src/main.tsx
@@ -9,12 +9,12 @@
 //   </React.StrictMode>,
 // )
 import { StrictMode } from 'react'
- import { createRoot } from 'react-dom/client'
- import './index.css'
- import App from './App.tsx'
- 
- createRoot(document.getElementById('root')!).render(
-   <StrictMode>
-     <App />
-   </StrictMode>,
- )
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.tsx'
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)
