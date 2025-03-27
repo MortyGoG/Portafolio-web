d74499872efc7bd7a506d3dbd12730e45bb7f45b
Morty <drakozack31@gmail.com>
2025-03-26 21:43:41 -0600

    deploy config

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,20 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+// import React from 'react'
+// import ReactDOM from 'react-dom/client'
+// import './index.css'
+// import App from './App.tsx'
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+// ReactDOM.createRoot(document.getElementById('root')!).render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+// )
+import { StrictMode } from 'react'
+ import { createRoot } from 'react-dom/client'
+ import './index.css'
+ import App from './App.tsx'
+ 
+ createRoot(document.getElementById('root')!).render(
+   <StrictMode>
+     <App />
+   </StrictMode>,
+ )
--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/mi-portafolio/',
+  base: process.env.NODE_ENV === 'production' ? '/mi-portafolio/' : '/',
   plugins: [react(), tailwindcss(),],
   
 })
