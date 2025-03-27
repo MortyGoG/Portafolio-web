fc80e08a3ca5ded68a8dad91dccec5940f20c21c
Morty <drakozack31@gmail.com>
2025-03-26 21:08:24 -0600

    deploy config

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/Portafolio-web/',
+  base: '/mi-portafolio/',
   plugins: [react(), tailwindcss(),],
   
 })
